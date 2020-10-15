6498d0610985c0f567fa18ba16b79cf525ab8e3d
Parker Manning <parkerlmanning@hotmail.com>
2020-10-15 02:35:15 -0500

    working inquirier prompts for view and add, console logging the appropriate information

--- db/seed.sql
+++
@@ -1,8 +0,0 @@
-
--- Schema for inserting the 'data' into the tables 
-INSERT INTO company (department, roles, employees) VALUES ('JCPenny', 'Clothing Store', 155);
-INSERT INTO company (department, roles, employees) VALUES ('Walmart', 'General Store', 1929);
-INSERT INTO company (department, roles, employees) VALUES ('Home-Depot', 'Hard-Ware Store', 440);
-INSERT INTO company (department, roles, employees) VALUES ('Target', 'General Store', 200);
-INSERT INTO company (department, roles, employees) VALUES ('TJ-Maxx', 'Clothing Store',410);
-INSERT INTO company (department, roles, employees) VALUES ('Ben-Jerrys', 'Ice-Cream', 10000);
